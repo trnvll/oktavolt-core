b87673e96b1dcbee02a2fc8186d07028517da14d
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2024-05-11 13:26:19 +0200

    chore: updating typescript version and using helmet for security

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,28 @@
-import { DatabaseExceptionFilter } from '@/filters/database-exception.filter'
-
 if (!process.env.IS_TS_NODE) {
   require('module-alias/register')
 }
 
 import { NestFactory } from '@nestjs/core'
+import { DatabaseExceptionFilter } from '@/filters/database-exception.filter'
+import helmet from '@fastify/helmet'
 import { ValidationPipe, VersioningType } from '@nestjs/common'
 
 import { AppModule } from '@/app.module'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log'],
-  })
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      logger: ['error', 'warn', 'log'],
+    },
+  )
 
+  await app.register(helmet)
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
   app.useGlobalFilters(new DatabaseExceptionFilter())
