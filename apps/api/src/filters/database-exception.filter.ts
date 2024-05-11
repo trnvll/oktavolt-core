dae64b42d88df9b8a11d5de418a04b28c44d78e3
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2024-05-11 12:46:35 +0200

    chore(logging): database filter exceptions

--- apps/api/src/filters/database-exception.filter.ts
+++ apps/api/src/filters/database-exception.filter.ts
@@ -3,14 +3,18 @@ import {
   Catch,
   ExceptionFilter,
   HttpStatus,
+  Logger,
 } from '@nestjs/common'
 import { PostgresError } from 'postgres'
 
 @Catch(PostgresError)
 export class DatabaseExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(DatabaseExceptionFilter.name)
+
   catch(exception: PostgresError, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
     const response = ctx.getResponse()
+    const request = ctx.getRequest()
     let status = HttpStatus.INTERNAL_SERVER_ERROR
 
     let message = 'Internal server error'
@@ -21,10 +25,16 @@ export class DatabaseExceptionFilter implements ExceptionFilter {
         'Duplicate entry error. The data you are trying to insert already exists.'
     }
 
+    this.logger.error(
+      `HTTP ${status} Response: ${message}`,
+      exception.stack,
+      `${request.method} ${request.url}`,
+    )
+
     response.status(status).json({
       statusCode: status,
       timestamp: new Date().toISOString(),
-      path: ctx.getRequest().url,
+      path: request.url,
       message: message,
     })
   }
