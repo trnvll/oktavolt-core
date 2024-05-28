d7b7fe166af790bbe04cc386867609cde979b43e
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2024-05-28 09:51:25 +0200

    perf(fastify): switching to use fastify + test config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,15 +4,25 @@ if (!process.env.IS_TS_NODE) {
 
 import { NestFactory } from '@nestjs/core'
 import { DatabaseExceptionFilter } from '@/filters/database-exception.filter'
-import helmet from 'helmet'
 import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from '@/app.module'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
+import helmet from '@fastify/helmet'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log'],
-  })
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+    {
+      logger: ['error', 'warn', 'log'],
+    },
+  )
 
   const config = new DocumentBuilder()
     .setTitle('Oktavolt API Specification')
@@ -23,7 +33,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
 
-  app.use(helmet())
+  await app.register(helmet)
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
   app.useGlobalFilters(new DatabaseExceptionFilter())
