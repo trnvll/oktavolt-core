5c64269bad323a29adec938bf37b40a03af177bd
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2024-05-28 21:26:49 +0200

    feat(events): fastify, caching, throttling, versioning etc

--- apps/events/src/app.module.ts
+++ apps/events/src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common'
 import { EventsModule } from '@/modules/events/events.module'
+import { CacheModule, CacheInterceptor } from '@nestjs/cache-manager'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core'
 
 @Module({
-  imports: [EventsModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    EventsModule,
+    CacheModule.register(),
+    ThrottlerModule.forRoot([
+      {
+        name: 'medium',
+        ttl: 10_000,
+        limit: 200,
+      },
+      {
+        name: 'long',
+        ttl: 60_000,
+        limit: 500,
+      },
+    ]),
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- apps/events/src/main.ts
+++ apps/events/src/main.ts
@@ -2,16 +2,44 @@ if (!Boolean(process.env.IS_TS_NODE)) {
   require('module-alias/register')
 }
 
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@/app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import helmet from '@fastify/helmet'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+    {
+      logger: ['error', 'warn', 'log'],
+    },
+  )
 
+  const config = new DocumentBuilder()
+    .setTitle('Oktavolt Events API Specification')
+    .setVersion('1.0')
+    .build()
+
+  // await SwaggerModule.loadPluginMetadata(metadata)
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.register(helmet)
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  })
 
-  await app.listen(Number(process.env.PORT) || 8080)
+  await app.listen(process.env.PORT || 8080, '0.0.0.0')
 }
 void bootstrap()
