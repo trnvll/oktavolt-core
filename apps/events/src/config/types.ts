65cee0fbfbc584d24b2b2f3377261ab8900df0ab
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2024-06-15 22:41:32 +0200

    chore: remove jest config and updating root config

--- apps/events/src/config/types.ts
+++ apps/events/src/config/types.ts
@@ -1,4 +1,7 @@
 import { RootBaseConfig } from '@/config/base.config'
 import { RootDatabaseConfig } from '@/config/database.config'
+import { RootExternalConfig } from '@/config/external.config'
 
-export type RootConfig = RootBaseConfig & RootDatabaseConfig
+export type RootConfig = RootBaseConfig &
+  RootDatabaseConfig &
+  RootExternalConfig
