a7ef554bc918ca03d589a7f4128af97fe85fb2ee
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2024-07-27 13:22:19 +0200

    feat(openapi): file to generate openapi json (not working currently)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { DatabaseExceptionFilter } from '@/filters/database-exception.filter'
 import { ValidationPipe, VersioningType } from '@nestjs/common'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from '@/app.module'
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify'
 import helmet from '@fastify/helmet'
-// import metadata from '@/metadata'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -20,15 +18,6 @@ async function bootstrap() {
       logger: ['error', 'warn', 'log'],
     },
   )
-
-  const config = new DocumentBuilder()
-    .setTitle('Oktavolt API Specification')
-    .setVersion('1.0')
-    .build()
-
-  // await SwaggerModule.loadPluginMetadata(metadata)
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
 
   await app.register(helmet)
   app.enableCors()
