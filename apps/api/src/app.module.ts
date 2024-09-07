8fe800399ebb2c946bf7c1cadacf0efa78eeaa4e
Alexander Törnvall <alexander.tornvall@roycgroup.com>
2024-09-07 12:57:03 +0200

    chore(bull): remove redis and bull deps

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -9,30 +9,21 @@ import { EventsModule } from '@/core/events/events.module'
 import { SqsModule } from '@/core/sqs/sqs.module'
 import { LlmModule } from '@/core/llm/llm.module'
 import { NotificationsModule } from '@/core/notifications/notifications.module'
-import { BullModule, BullModuleOptions } from '@nestjs/bull'
-import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config'
 import { baseConfig } from '@/config/base.config'
 import { awsConfig } from '@/config/aws.config'
 import { databaseConfig } from '@/config/database.config'
-import { RedisConfig, redisConfig } from '@/config/redis.config'
 import { externalConfig } from '@/config/external.config'
 import { EmbeddingsModule } from '@/modules/embeddings/embeddings.module'
 import { ChatsModule } from '@/modules/chats/chats.module'
 import { ToolExecsModule } from '@/modules/tool-execs/tool-execs.module'
-import { json } from 'shared'
 import { SlackModule } from '@/modules/slack/slack.module'
 import { ResourcesModule } from '@/modules/resources/resources.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [
-        baseConfig,
-        awsConfig,
-        redisConfig,
-        externalConfig,
-        databaseConfig,
-      ],
+      load: [baseConfig, awsConfig, externalConfig, databaseConfig],
       isGlobal: true,
       cache: true,
     }),
@@ -50,35 +41,6 @@ import { ResourcesModule } from '@/modules/resources/resources.module'
       },
     ]),
     EventEmitterModule.forRoot(),
-    BullModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (
-        configService: ConfigService,
-      ): Promise<BullModuleOptions> => {
-        const redisConfig = configService.getOrThrow<RedisConfig>('redis')
-
-        const { url, host, port } = redisConfig
-
-        console.log('Redis config:', json(redisConfig))
-
-        if (!url) {
-          throw new Error(
-            `Redis configuration is missing. Got ${json(redisConfig)}`,
-          )
-        }
-
-        return {
-          redis: {
-            family: 6,
-            maxRetriesPerRequest: 5,
-            port,
-            host,
-          },
-          // url,
-        }
-      },
-    }),
     AuthzModule,
     UsersModule,
     EventsModule,
